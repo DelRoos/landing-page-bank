637e81a6b076c25fc100814f752f156d96de3166
DelRoos <delanoroosvelt733@gmail.com>
2021-12-22 17:54:05 +0100

    Add fade in fade out animation on menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const btnHambubger = document.querySelector("#btnHambuger");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+
+btnHambubger.addEventListener("click", function () {
+    console.log("click");
+    if(header.classList.contains("open")){
+        header.classList.remove("open");
+        overlay.classList.remove("fade-in");
+        overlay.classList.add("fade-out");
+    }else{
+        header.classList.add("open");
+        overlay.classList.remove("fade-out");
+        overlay.classList.add("fade-in");
+    }
+})
