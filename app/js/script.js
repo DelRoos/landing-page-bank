4f9623e9464f238c6484c7bea8a0b85cde0624ae
DelRoos <delanoroosvelt733@gmail.com>
2021-12-22 19:02:35 +0100

    Add mobile  menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,24 @@
 const btnHambubger = document.querySelector("#btnHambuger");
+const body = document.querySelector('body');
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHambubger.addEventListener("click", function () {
     console.log("click");
     if(header.classList.contains("open")){
+        body.classList.remove("noscroll");
         header.classList.remove("open");
-        overlay.classList.remove("fade-in");
-        overlay.classList.add("fade-out");
+        fadeElems.forEach(function (element){
+            element.classList.add("fade-out");
+            element.classList.remove("fade-in");
+        });
     }else{
+        body.classList.add("noscroll");
         header.classList.add("open");
-        overlay.classList.remove("fade-out");
-        overlay.classList.add("fade-in");
+        fadeElems.forEach(function (element){
+            element.classList.add("fade-in");
+            element.classList.remove("fade-out");
+        });
     }
 })
